d1399bd1d256c47ab949647958c9f8f22721e148
lallison <lallison@student.21-school.ru>
2022-07-20 15:35:12 +0300

    Day01/ex00

---
+++ Day01/ex00/Program.java
@@ -0,0 +1,50 @@
+public class Program {
+	public static void main(String[] args) {
+		User user1 = new User(1, "Romul", 20000);
+		User user2 = new User(2, "Richard", 40000);
+		User user3 = new User(3, "Kamil", 90000);
+
+		System.out.println("Used id:\t" + user1.getId() + "\tUser name:\t" + user1.getName()
+				+ "\tUser balance:\t$" + user1.getBalance());
+		System.out.println("Used id:\t" + user2.getId() + "\tUser name:\t" + user2.getName()
+				+ "\tUser balance:\t$" + user2.getBalance());
+		System.out.println("Used id:\t" + user3.getId() + "\tUser name:\t" + user3.getName()
+				+ "\tUser balance:\t$" + user3.getBalance());
+
+		System.out.println();
+
+		Transaction transaction1 = new Transaction(user2, user3, Transaction.Category.DEBIT, 30000);
+		System.out.println(transaction1.getId());
+		System.out.println(transaction1.getRecipient().getId());
+		System.out.println(transaction1.getSender().getId());
+		System.out.println(transaction1.getCategory());
+		System.out.println(transaction1.getAmount());
+
+		System.out.println();
+
+		Transaction transaction2 = new Transaction(user1, user3, Transaction.Category.DEBIT, -30000);
+		System.out.println(transaction2.getId());
+		System.out.println(transaction2.getRecipient().getId());
+		System.out.println(transaction2.getSender().getId());
+		System.out.println(transaction2.getCategory());
+		System.out.println(transaction2.getAmount());
+
+		System.out.println();
+
+		Transaction transaction3 = new Transaction(user1, user3, Transaction.Category.CREDIT, 30000);
+		System.out.println(transaction3.getId());
+		System.out.println(transaction3.getRecipient().getId());
+		System.out.println(transaction3.getSender().getId());
+		System.out.println(transaction3.getCategory());
+		System.out.println(transaction3.getAmount());
+
+		System.out.println();
+
+		Transaction transaction4 = new Transaction(user1, user3, Transaction.Category.CREDIT, -30000);
+		System.out.println(transaction4.getId());
+		System.out.println(transaction4.getRecipient().getId());
+		System.out.println(transaction4.getSender().getId());
+		System.out.println(transaction4.getCategory());
+		System.out.println(transaction4.getAmount());
+	}
+}
---
+++ Day01/ex00/User.java
@@ -0,0 +1,38 @@
+public class User {
+	private Integer id;
+	private String name;
+	private Integer balance;
+
+	public User (Integer id, String name, Integer balance) {
+		this.id = id;
+		this.name = name;
+		this.balance = balance;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Integer balance) {
+		if (balance < 0)
+			this.balance = 0;
+		else
+			this.balance = balance;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
