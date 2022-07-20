78c5e1293b2a938d9fff14c786a8900de9fcad3b
lallison <lallison@student.21-school.ru>
2022-07-20 20:53:40 +0300

    Day01/ex04

---
+++ Day01/ex04/TransactionsList.java
@@ -0,0 +1,9 @@
+import java.util.UUID;
+
+interface TransactionsList {
+
+    void addTransaction(Transaction transaction);
+    void removeTransactionById(UUID uuid);
+    Transaction[] toArray();
+    Integer getSize();
+}
