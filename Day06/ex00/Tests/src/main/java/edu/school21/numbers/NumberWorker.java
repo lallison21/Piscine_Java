41b3a1735bc695fb1165d5c9576aa0f2b967ac80
lallison <lallison@student.21-school.ru>
2022-07-27 12:55:25 +0300

    Day06/ex00

---
+++ Day06/ex00/Tests/src/main/java/edu/school21/numbers/NumberWorker.java
@@ -0,0 +1,35 @@
+package edu.school21.numbers;
+
+public class NumberWorker {
+
+	public boolean isPrime(int number) {
+
+		if (number < 2) {
+			throw new IllegalNumberException("Number < 2");
+		}
+		int i;
+
+		for (i = 2; i * i <= number; i++) {
+			if (number % i == 0) {
+				break ;
+			}
+		}
+
+		if (i * i > number || number == 3) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int digitsSum(int number) {
+
+		int result;
+
+		for (result = 0; number > 0; number /= 10) {
+			result += number % 10;
+		}
+		return result;
+	}
+}
+
