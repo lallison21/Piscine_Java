8a793692033b484565eec7bd651e65484c947e02
lallison <lallison@student.21-school.ru>
2022-07-29 17:03:43 +0300

    Day08/ex00

--- Day08/ex00/Spring/src/main/java/app/Program.java
+++ Day08/ex00/Spring/src/main/java/app/Program.java
@@ -3,12 +3,15 @@ package app;
 import printer.Printer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import printer.PrinterWithDateTimeImpl;
+import printer.PrinterWithPrefixImpl;
 
 public class Program {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-		Printer printer = context.getBean("prefix", Printer.class);
+		PrinterWithPrefixImpl printer = context.getBean("prefix", PrinterWithPrefixImpl.class);
+		printer.setPrefix("Prefix ");
 		printer.print ("Hello!") ;
 	}
 }
